0bae036180d7824bad3bac8e38fb2fb306ad0374
Aishaml1 <Aisha.lugg1@icloud.com>
2021-10-15 09:55:46 -0400

    questions

---
+++ quiz-questions.js
@@ -0,0 +1,22 @@
+const music = [`finish the lyrics. 'You make my earth quake, ridin' around______________. -'Earthquake'. Tyler the creator ?`,
+`Kanye West sampled Lauryn Hill's 'Doo Wop (That Thing)' on Donda, what song was it?`,
+`Finish the lyrics. "So let's set the world on fire ___________________."   -'We are Young'. fun.`,
+`?`,
+`Justin bieber has a song called 'Peaches' featuring what other two artist ? `,
+`Finish the lyrics. 'call me by your name, tell me you love me in private ____________________. -'MONTERO'. Lil Nas X `,
+`Choose the correct lyrics to the song Essence by Wizkid ft Tems `,
+``
+]
+
+const movies = []
+const videoGames = [
+    'What is the all time top selling nintendo switch game ?', //mario kart 8 delux
+    'Whats are the main characters in kingdom hearts?',
+    'how many characters are there in Super Smash Bros?',
+    'what was the name of the first Grand Theft Auto (GTA)?',
+    'What is the currency in Fornite?',
+    `what athlete was on the cover of legend edition 2k20? `,
+    `who is Clanks counterpart in the game Ratchet & Clank: Rift Apart ?`,
+    `What is being collected to determine a winner in the game Super Mario Party? `
+]
+const random = []
